95fd043600b7752b981b8e64b52c16de58cf4c3e
nazulaa <rifdanajlaazzahra@gmail.com>
2026-01-24 18:07:16 +0700

    progress2

--- animation.js
+++ animation.js
@@ -87,3 +87,4 @@ scrollContainer.addEventListener("mouseenter", () => isPaused = true);
 scrollContainer.addEventListener("mouseleave", () => isPaused = false);
 scrollContainer.addEventListener("touchstart", () => isPaused = true);
 scrollContainer.addEventListener("touchend", () => isPaused = false);
+
